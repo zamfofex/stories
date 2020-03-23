2755295aa0bbded44f7c719af2b8a6c7c3b3bffc
Zambonifofex <zambonifofex@gmail.com>
2020-03-22 23:37:00 -0300

    fix minor exploit

--- api/feedback.js
+++ api/feedback.js
@@ -3,6 +3,13 @@ let {MongoClient} = mongodb
 
 export default async ({query: {name}, body: {message}}, res) =>
 {
+	if (!message || !name)
+	{
+		res.statusCode = 400
+		res.end()
+		return
+	}
+	
 	let mongo = await MongoClient.connect(process.env.mongo_url)
 	
 	await mongo.db(process.env.mongo_database).collection("stories").updateOne({name}, {$push: {feedback: {date: new Date(), message}}})
