5506619f37488c0f7f550f1afec7dd40a8ec8205
Zambonifofex <zambonifofex@gmail.com>
2020-05-24 22:55:13 -0300

    fix changing themes with keyboard

--- scripts/themes.js
+++ scripts/themes.js
@@ -2,7 +2,7 @@ let theme = document.querySelector("#theme")
 let select = theme.querySelector("select")
 theme.classList.remove("disabled")
 select.disabled = false
-select.addEventListener("click", () => document.body.dataset.theme = select.value)
+select.addEventListener("change", () => document.body.dataset.theme = select.value)
 
 CSS.registerProperty({name: "--text-color", syntax: "<color>", inherits: true, initialValue: "#0000"})
 CSS.registerProperty({name: "--background-color", syntax: "<color>", inherits: true, initialValue: "#0000"})
