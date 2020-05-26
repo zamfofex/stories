f4031fce29d9d09af28969748345756bacdc299d
Zambonifofex <zambonifofex@gmail.com>
2020-05-26 10:51:10 -0300

    check for ‘CSS.registerProperty’ before using it

--- scripts/themes.js
+++ scripts/themes.js
@@ -4,5 +4,8 @@ theme.classList.remove("disabled")
 select.disabled = false
 select.addEventListener("change", () => document.body.dataset.theme = select.value)
 
-CSS.registerProperty({name: "--text-color", syntax: "<color>", inherits: true, initialValue: "#0000"})
-CSS.registerProperty({name: "--background-color", syntax: "<color>", inherits: true, initialValue: "#0000"})
+if (CSS.registerProperty)
+{
+	CSS.registerProperty({name: "--text-color", syntax: "<color>", inherits: true, initialValue: "#0000"})
+	CSS.registerProperty({name: "--background-color", syntax: "<color>", inherits: true, initialValue: "#0000"})
+}
