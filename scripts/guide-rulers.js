d4e6a4e320d40bd5430594c94966865066d09d07
Zambonifofex <zambonifofex@gmail.com>
2020-07-01 11:20:11 -0300

    fix “guide rulers” feature

--- scripts/guide-rulers.js
+++ scripts/guide-rulers.js
@@ -2,7 +2,7 @@ let group = document.querySelector("#guide-rulers")
 
 group.classList.remove("disabled")
 
-for (let radio of group.querySelector("input"))
+for (let radio of group.querySelectorAll("input"))
 {
 	radio.disabled = false
 	radio.addEventListener("change", () => document.body.setAttribute("data-guide-rulers", radio.value))
