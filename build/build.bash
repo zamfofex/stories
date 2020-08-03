8cc67dc2f678489a07461921bba2f80d7e5c1a5c
Zambonifofex <zambonifofex@gmail.com>
2020-08-03 10:50:49 -0300

    fix script linking

--- build/build.bash
+++ build/build.bash
@@ -2,10 +2,11 @@
 
 set -e
 
-mkdir -p public
+mkdir -p public/scripts
 rm -rf public/*
 
-ln style.css script.js scripts/* public
+ln style.css script.js public
+ln scripts/* public/scripts
 ln not-found/main.svg public/not-found.svg
 ln not-found/main.html public/not_found.html
 
