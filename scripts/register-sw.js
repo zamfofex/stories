e0fcf0c22c0e2f8cd29e2f66cba392842ad1c8e1
Zambonifofex <zambonifofex@gmail.com>
2020-08-22 07:02:01 -0300

    fix service worker registration in Chrome

--- scripts/register-sw.js
+++ scripts/register-sw.js
@@ -1,3 +1,3 @@
 import "./indexed-db.js"
 // Also load ‘dependencies.js’ so dependencies can be cached from the list page.
-navigator.serviceWorker.register("/sw.js", {type: "module"}).then(() => import("./dependencies.js"))
+navigator.serviceWorker.register("/sw.js").then(() => import("./dependencies.js"))
