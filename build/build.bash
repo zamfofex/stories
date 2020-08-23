82c0ec3859924e9a67f7028476c2b9b3a936486d
Zamfofex <zambonifofex@gmail.com>
2020-08-23 19:58:59 -0300

    add favicon (closes #7)

--- build/build.bash
+++ build/build.bash
@@ -5,7 +5,7 @@ set -e
 rm -rf public/*
 mkdir -p public/scripts
 
-ln style.css script.js dependencies.css public
+ln style.css script.js dependencies.css icon.svg public
 ln scripts/* public/scripts
 
 node build/build.js
