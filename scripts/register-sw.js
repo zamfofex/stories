9549631389f579413b4642dddde3ab05df35d070
Zambonifofex <zambonifofex@gmail.com>
2020-09-15 23:33:54 -0300

    allow settings to be used in browsers without dynamic imports

--- scripts/register-sw.js
+++ scripts/register-sw.js
@@ -1,3 +1,3 @@
 import "./indexed-db.js"
 // Also load ‘dependencies.js’ so dependencies can be cached from the list page.
-navigator.serviceWorker.register("/sw.js").then(() => import("./dependencies.js"))
+navigator.serviceWorker.register("/sw.js").then(Function(`import("./dependencies.js")`))
