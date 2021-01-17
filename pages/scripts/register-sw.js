beac4cb4d81877b7e9cd4948e0443f57e99bcc0f
Zamfofex <zambonifofex@gmail.com>
2021-01-17 18:57:52 -0300

    fix service worker registration
    Some browsers elide the ‘serviceWorker’ property in some contexts.

--- pages/scripts/register-sw.js
+++ pages/scripts/register-sw.js
@@ -1,2 +1,2 @@
 // Also load ‘dependencies.js’ so dependencies can be cached from the list page.
-navigator.serviceWorker.register("/sw.js").then(Function(`import("./dependencies.js")`))
+if (navigator.serviceWorker) navigator.serviceWorker.register("/sw.js").then(Function(`import("./dependencies.js")`))
