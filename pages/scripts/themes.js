86cd62cd47299f6d924289a7eed25001a1306a8f
Zamfofex <zambonifofex@gmail.com>
2021-02-02 09:05:09 -0300

    define custom CSS properties in CSS

--- pages/scripts/themes.js
+++ pages/scripts/themes.js
@@ -1,9 +1,3 @@
 import {subscribe} from "./messages.js"
 
 subscribe("theme", theme => document.documentElement.dataset.theme = theme)
-
-if (CSS.registerProperty)
-{
-	CSS.registerProperty({name: "--text-color", syntax: "<color>", inherits: true, initialValue: "#0000"})
-	CSS.registerProperty({name: "--background-color", syntax: "<color>", inherits: true, initialValue: "#0000"})
-}
